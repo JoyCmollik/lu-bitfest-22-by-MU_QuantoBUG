f4a208608420f2a6a621cb2042d44bca80e1bbee
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-09-11 15:43:01 +0600

    added admin, consumer dashbaord

--- client-side/src/Pages/Dashboard/DashboardHome/DashboardHome.js
+++ client-side/src/Pages/Dashboard/DashboardHome/DashboardHome.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Routes } from 'react-router-dom';
+import AdminDashboardHome from '../AdminDashboard/AdminDashboardHome/AdminDashboardHome';
+import DashboardSidebar from '../DashboardSidebar/DashboardSidebar'
 
 const DashboardHome = () => {
   return (
-    <div className="grid grid-cols-12">
-        <div className="col-span-2 bg-primary">
-            sidebar
-        </div>
-        <div className="col-span-10 bg-dark">content</div>
-    </div>
-  )
+		<div className=''>
+            <div className="h-[100px] bg-dark"></div>
+			<div className='grid grid-cols-12 px-[25px] bg-light'>
+				<div className='col-span-3 bg-light rounded-xl -mt-[50px]'>
+					<DashboardSidebar />
+				</div>
+				<div className='col-span-9'>
+                    <AdminDashboardHome />
+                </div>
+			</div>
+		</div>
+  );
 }
 
 export default DashboardHome
