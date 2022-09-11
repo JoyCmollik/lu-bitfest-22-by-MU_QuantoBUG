370b1c6334ad4d058140720a6c397a0f36b14a32
shahidamahtab <marwa.mahtab@gmail.com>
2022-09-11 15:23:25 +0600

    Consumer and Admin Register

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,8 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import DashboardHome from './Pages/Dashboard/DashboardHome/DashboardHome';
 import Home from './Pages/HomePage/Home/Home';
-import Login from './Pages/LoginPage/Login/Login';
-import Register from './Pages/LoginPage/Register/Register';
+import AdminLogin from './Pages/LoginPage/AdminLogin/AdminLogin';
+import AdminRegister from './Pages/LoginPage/AdminRegister/AdminRegister';
+import ConsumerLogin from './Pages/LoginPage/ConsumerLogin/ConsumerLogin';
+import ConsumerRegister from './Pages/LoginPage/ConsumerRegister/ConsumerRegister';
+
 import NotFound from './Pages/NotFoundPage/NotFound/NotFound';
 
 function App() {
@@ -12,8 +15,13 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Home />}></Route>
 				<Route path='/home' element={<Home />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/register' element={<Register />} />
+				<Route path='/adminlogin' element={<AdminLogin />} />
+				<Route path='/adminRegister' element={<AdminRegister />} />
+				<Route
+					path='/consumerRegister'
+					element={<ConsumerRegister />}
+				/>
+				<Route path='/consumerLogin' element={<ConsumerLogin />} />
 				<Route path='/dashboard' element={<DashboardHome />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
