477c8f76e1740b44de0164da400da90f7e9c3ae3
shahidamahtab <marwa.mahtab@gmail.com>
2022-09-11 21:18:44 +0600

    update

--- client-side/src/Pages/Dashboard/AdminDashboard/AdminBusInventory/AdminBusInventory.js
+++ client-side/src/Pages/Dashboard/AdminDashboard/AdminBusInventory/AdminBusInventory.js
@@ -5,9 +5,9 @@ const AdminBusInventory = () => {
 	return (
 		<section>
 			<div>
-				<h2>
+				<h2 className='border-2 border-dark inline-block py-2 px-4 rounded-md hover:bg-dark text-dark font-semibold hover:text-white'>
 					<span>
-						<MdOutlineLibraryAdd className='inline mr-2 text-2xl' />
+						<MdOutlineLibraryAdd className='inline mr-2 text-3xl' />
 					</span>
 					Add Bus
 				</h2>
