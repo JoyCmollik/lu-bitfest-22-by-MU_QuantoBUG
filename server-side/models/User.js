2239242b55accd5616455962076241915355d839
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-09-11 20:25:43 +0600

    refactored authentication in front-end, back-end

--- server-side/models/User.js
+++ server-side/models/User.js
@@ -50,6 +50,10 @@ UserSchema.pre('save', async function () {
 	this.password = await bcrypt.hash(this.password, salt);
 });
 
+UserSchema.methods.addStudentCredentials = function({}) {
+	
+};
+
 UserSchema.methods.comparePassword = async function (candidatePassword) {
 	const isMatch = await bcrypt.compare(candidatePassword, this.password);
 	return isMatch;
